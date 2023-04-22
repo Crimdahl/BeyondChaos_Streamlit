dc0883c56549112850e6f14bb3a27b8d39f1433e
Crimdahl <crimdahl@outlook.com>
2023-04-22 07:28:51 -0400

    0.1.2.1: Adjusted removeflashing flag
    - removeflashing is now a combobox. Users can choose to remove all flashing or just the flashing from Bum Rush and Duncan's Bum Rush cutscene.

--- 1_Welcome.py
+++ 1_Welcome.py
@@ -43,7 +43,7 @@ def main():
     )
     set_stylesheet()
     sl.title("Beyond Chaos: Web Edition")
-    sl.markdown('<p style="font-size: 14px; margin-top: -20px;font-family: Arial;">Version 0.1.2.0</p>',
+    sl.markdown('<p style="font-size: 14px; margin-top: -20px;font-family: Arial;">Version 0.1.2.1</p>',
                 unsafe_allow_html=True)
 
     if "initialized" not in sl.session_state.keys():
