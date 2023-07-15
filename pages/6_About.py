cf20bd98c409fed805253c2d13b0dc2afe25beb6
Crimdahl <crimdahl@outlook.com>
2023-07-15 16:06:41 -0400

    Bumped version (stop forgetting!)

--- pages/6_About.py
+++ pages/6_About.py
@@ -849,6 +849,22 @@ def main():
         # Populate the Changelog tab
         #
         with tabs[2].expander(
+                label='Version 0.3.3.3: CSS Formatting modifications for better 1080p viewing',
+                expanded=False
+        ):
+            sl.markdown(
+                "<ul>"
+                '<li>Reduced top and bottom padding app-wide</li>'
+                '<li>On the remonsterate screen: Shrunk the left column. Wxpanded the left column. '
+                'Floated the Folder and Image drop-down selectors.</li>'
+                '<li>Fixed a bug where leaving and going back to the Remonsterate screen could show the wrong sprite '
+                'being rendered because the drop-downs do not remember the selection. Maybe I\'ll give the drop-downs '
+                'memory later.</li>'
+                "</ul><br>",
+                unsafe_allow_html=True
+            )
+
+        with tabs[2].expander(
                 label='Version 0.3.3.2: Corrected remonsterate sprite file names',
                 expanded=False
         ):
